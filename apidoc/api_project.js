c35b74005900d806e07f230e2c3ec56b7711c131
zhub <zhub@corp.ecc.kz>
2019-09-14 00:16:16 +0600

    fix api description

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,7 +1,7 @@
 define({
   "name": "api_zandylyq",
   "version": "0.1.0",
-  "description": "Documentation for API of api.zandylyq.kz",
+  "description": "Documentation for api.zandylyq.kz",
   "title": "API zandylyq",
   "url": "https://api.zandylyq.kz/v1",
   "sampleUrl": false,
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-09-13T18:07:23.622Z",
+    "time": "2019-09-13T18:15:25.550Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
